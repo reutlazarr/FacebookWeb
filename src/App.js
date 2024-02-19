be3506a3b61e6b599cafc28a3ae01e41c72d2d86
reutlazar <reootlazar@gmail.com>
2024-02-19 12:33:18 +0200

    update app and index.html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css';
-import Login from './screens/Login';
-import Registration from './screens/Registration';
+import React, { useState } from "react";
+import "./App.css";
+import Login from "./screens/Login";
+import Registration from "./screens/Registration";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Feed from "./screens/Feed";
 
 function App() {
+  const [user, setUser] = useState(null); // Initialize user state to null
+
   return (
     <div className="container-fluid">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Registration />} />
-          <Route path='/signIn' element={Feed />} />
+          <Route path="/signIn" element={<Feed user={user} />} />
         </Routes>
       </BrowserRouter>
     </div>
