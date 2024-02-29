89a18260de27dd2cda3b2483ddd1d8ee080d541b
BarVinizky <barr98@gmail.com>
2024-02-29 11:44:54 +0200

    update form

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,86 +4,103 @@ import './Login.css';
 import { getUser, validateUserData, validateUserEmail } from '../utils/Utils';
 
 const Login = ({ setUser, setToken }) => {
-    //const [user, setUser] = useState(null);
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
     const [validated, setValidated] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        emailError: "",
+        passwordError: "",
+    });
   
     function setFinalUser(setUser, credentials) {
         setUser({
           email: credentials.email
         });
     }
+
+    // handle user input changes
+    const handleChange = (e) => {
+        const { name, value, checked } = e.target; // Get the event targt
+        setFormData((prevData) => ({
+        ...prevData,
+        [name]: value, // Update the names in prevData with thier new value
+        [`${name}Error`]: "",
+        }));
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setEmailError('');
-        setPasswordError('');
-        
-        const credentials = {
-            email: email,
-            password: password
-        }
 
-        if (!email) {
-            setEmailError('Email is required');
-            setValidated(true);
-            return;
-        }
-        // Find user with matching email
-        // if (validateUserData(email, password)) {
-        //     // Adjust as per your app's routing
-        //     setValidated(false);
-        //     setFinalUser(setUser, getUser(email, password))
-        //     //setUser(getUser(email, password));
-        //     navigate('/signIn');
-        //     //alert('Login succesful');
-        // } 
-        try {
-            const response = await fetch('http://localhost:8080/api/tokens', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(credentials),
-            });
-            if (!response.ok) {
-                if (response.status === 409) {
-                    setEmailError('Email not found');
-                    setValidated(true);
-                    return;
-                }
-                throw new Error('Failed to log in');
+        // Validate form fields not empty
+        const validateField = (field, errorMessage) => {
+            if (!formData[field].trim()) {
+            errors[`${field}Error`] = errorMessage;
+            isValid = false;
             }
-            const json = await response.json();
+        };
 
-            console.log("login " + json.token); // Process the response data, e.g., save the token
-            console.log("login " + credentials.email);
-            setToken(json.token);
-            setFinalUser(setUser, credentials);
-            //setUser(credentials.email);
-            setValidated(false);
+        // Validate form fields
+        let isValid = true;
+        const errors = {};
+        validateField("email", "Email address is required");
+        validateField("password", "Password is required");
 
-            navigate('/signIn');
-            // Redirect or manage login state
-        } catch (error) {
-            console.error(error);
+        // Set formData with the data and matching erors
+        setFormData((prevData) => ({
+            ...prevData,
+            ...errors,
+        }));
+
+        if (isValid) {
+            // Save user information
+            const userData = {
+              email: formData.email,
+              password: formData.password,
+            };
+
+            try {
+                const response = await fetch('http://localhost:8080/api/tokens', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(userData),
+                });
+                if (!response.ok) {
+                    if (response.status === 409) {
+                        errors.emailError = "Email not found";
+                        throw new Error('Email not found');
+                    }
+                    throw new Error('Failed to log in');
+                }
+                const json = await response.json();
+    
+                console.log("login " + json.token); // Process the response data, e.g., save the token
+                console.log("login " + userData.email);
+                setToken(json.token);
+                setFinalUser(setUser, userData);
+                // If the form is valid, reset the validation state and submit the form
+                setValidated(false);
+                // Clean
+                setFormData({
+                email: "",
+                password: "",
+                });
+                // Navigate to signIn
+                navigate('/signIn');
+            } catch (error) {
+                console.error(error);
+                isValid = false;
+                // Set formData with the data and matching erors
+                setFormData((prevData) => ({
+                ...prevData,
+                ...errors,
+                }));
+            }
         }
-        // if(!validateUserEmail(email)) {
-        //     setEmailError('Email not found');
-        //     setValidated(true);
-        //     return;
-        // } else if(!password) {
-        //     setPasswordError('Password is required');
-        //     setValidated(true);
-        //     return;
-        // } else {
-        //     setPasswordError('Invalid password');
-        //     setValidated(true);
-        //     return;
-        // }
+        // If the form is invalid, display validation feedback
+        setValidated(true);
     };
   
     return (   
@@ -94,39 +111,44 @@ const Login = ({ setUser, setToken }) => {
                 </div>
                 <div className="col-sm-6">
                     <div className="card text-center shadow p-4 mb-4 login-card">
-                        <form noValidate {...(validated ? { validated: 'true' } : {})} onSubmit={handleSubmit}>
-                            <div className="card-body">
+                        <div className="card-body"> 
+                            <form noValidate validated={validated} onSubmit={handleSubmit}> 
                                 <p className="form-floating mb-3">
                                     <input
                                         type="email"
-                                        className={`form-control ${validated && emailError ? 'is-invalid' : ''}`}
+                                        className={`form-control ${validated && formData.emailError ? 'is-invalid' : ''}`}
                                         id="floatingInput"
+                                        name="email"
                                         placeholder="name@example.com"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)} />
-                                    <label htmlFor="floatingInput">Email</label>
-                                    {emailError && <div className="invalid-feedback">{emailError}</div>}
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        required
+                                        autoComplete="Email" />
+                                    <label htmlFor="floatingInput" className="form-label-login">Email address</label>
+                                    <div className="invalid-feedback">{formData.emailError}</div>
                                 </p>
                                 <p className="form-floating mb-3">
                                     <input
                                         type="password"
-                                        className={`form-control ${validated && passwordError ? 'is-invalid' : ''}`}
+                                        className={`form-control ${validated && formData.passwordError ? 'is-invalid' : ''}`}
                                         id="floatingPassword"
+                                        name="password"
                                         placeholder="Password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)} />
-                                    <label htmlFor="floatingPassword">Password</label>
-                                    {passwordError && <div className="invalid-feedback">{passwordError}</div>}
+                                        value={formData.password}
+                                        onChange={handleChange}
+                                        required
+                                        autoComplete="password" />
+                                    <label htmlFor="floatingPassword" className="form-label-login">Password</label>
+                                    <div className="invalid-feedback">{formData.passwordError}</div>
                                 </p>
-                                
                                 <div class="d-grid gap-2">
                                     <button type="submit" className="btn btn-primary btn-lg">Log in</button>
                                 </div>
-                            </div>
+                            </form>
+                        </div>
                             <div className="card-footer text-body-secondary">
                                 <Link to="/register" className="btn btn-success">Create new account</Link>
                             </div>
-                        </form>
                     </div>
                 </div>
             </div>
