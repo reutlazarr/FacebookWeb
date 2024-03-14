d1fa2298111008a6b11b6ecd2ab12ceb3f199f05
Reut Lazar <reootlazar@gmail.com>
2024-03-14 19:27:03 +0200

    remove profile- it became UserProfile

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Registration from "./screens/Registration";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Feed from "./screens/Feed";
 import { useState } from "react";
-import Profile from "./screens/Profile";
 import UserProfile from "./screens/UserProfile";
+import FriendsPage from "./screens/FriendsPage";
+
 
 function App() {
   // const [token, setToken] = useState(null);
@@ -20,9 +21,8 @@ function App() {
             element={<Registration />}
           />
           <Route path="/signIn" element={<Feed user={user} />} />
-          <Route path="/Profile" element={<Profile user={user} />} />
           <Route path="/UserProfile" element={<UserProfile user={user} />} />
-          <Route path="/test" element={<div><h1>test</h1></div>} />
+          <Route path="/FriendsPage" element={<FriendsPage user={user} />} />
         </Routes>
       </BrowserRouter>
     </div>
